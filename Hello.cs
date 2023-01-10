e0a0482f88c4d499c665ec3eb38e130d29c244ee
AljazPotocnik <122385433+AljazPotocnik@users.noreply.github.com>
2023-01-10 18:35:36 +0100

    Update Hello.cs
    Razrešen konflikt

--- Hello.cs
+++ Hello.cs
@@ -3,10 +3,7 @@ public class Hello
    public static void Main()
    {
       System.Console.WriteLine("Hello, World!");
-<<<<<<< HEAD
+      System.Console.WriteLine("Hello, GitHub!");
       System.Console.WriteLine("Hello, Local!");
-=======
-      System.Console.WriteLine("Hello, GitHub!")
->>>>>>> 3f295478376499f30c7851176d29743d7daecd9e
    }
 }
